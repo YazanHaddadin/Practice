4c1fbd6ff64597b2aeb82a28d35ece0247fc3075
YazanHaddadin <yazanhaddadin7@gmail.com>
2020-05-20 21:53:21 -0700

    testing

---
+++ test_python.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestC(unittest.TestCase):
+
+    def summ(self, A, B):
+        return A * B
+
+    def test_method_trig(self):
+        assert self.summ(5,5) == 25, "NO"
+        #self.assertEqual(self.summ(5,5) , 10, "should be 10")
+
+    def test_bad(self):
+        data = "OK"
+        with self.assertRaises(TypeError):
+            res = self.summ(data, "SO")
+
+ 
